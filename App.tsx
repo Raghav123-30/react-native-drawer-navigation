e59fa2fe58d5ee3d68678af7787a0006d984c097
Raghav123-30 <Raghavendrabhat2018@gmail.com>
2024-01-28 19:43:44 +0530

    completed

--- App.tsx
+++ App.tsx
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./screens/HomeScreen";
+import OrdersScreen from "./screens/OrdersScreen";
+import AboutScreen from "./screens/AboutScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  const Drawer = createDrawerNavigator();
+  const DrawerGroup = () => {
+    return (
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#0f172b",
+          },
+          headerTintColor: "#ccc",
+
+          drawerContentStyle: {
+            backgroundColor: "#0f172a",
+          },
+          drawerItemStyle: {
+            backgroundColor: "#0f172a",
+          },
+          drawerActiveTintColor: "#f8fafc",
+          drawerInactiveTintColor: "#64748b",
+        }}
+      >
+        <Drawer.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size}></Ionicons>
+            ),
+          }}
+        ></Drawer.Screen>
+        <Drawer.Screen
+          name="Orders"
+          component={OrdersScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="cart" color={color} size={size}></Ionicons>
+            ),
+          }}
+        ></Drawer.Screen>
+        <Drawer.Screen
+          name="About"
+          component={AboutScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="information" color={color} size={size}></Ionicons>
+            ),
+          }}
+        ></Drawer.Screen>
+        <Drawer.Screen
+          name="Logout"
+          component={HomeScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="log-out" color={color} size={size}></Ionicons>
+            ),
+          }}
+        ></Drawer.Screen>
+      </Drawer.Navigator>
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <DrawerGroup />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
